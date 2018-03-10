152e5f3f5f0f19d1e092c98b859c5bb7ad95e747
olegsid <olegsid93@gmail.com>
2018-03-10 14:46:12 +0200

    update(*): unit switcher functionality

--- app/js/dev/WeatherApp.js
+++ app/js/dev/WeatherApp.js
@@ -1,13 +1,46 @@
+const units = {
+	CELSIUS: 'CELSIUS',
+	KELVIN: 'KELVIN'
+};
+
 class WeatherApp {
-	constructor(cityName, temperature, countryCode, weatherDescription) {
+	constructor(cityName, temperature, countryCode, weatherDescription, switcher, unitType = units.CELSIUS) {
 		this.cityName = cityName;
 		this.temperature = temperature;
 		this.countryCode = countryCode;
 		this.weatherDescription = weatherDescription;
+		this.switcher = switcher
+		this.unitType = unitType;
+
+		switcher.addEventListener('click', () => this.onSwitch())		
 	}
 
 	setProperty(property, value, decorator) {
 		property && displayData(this[property], value, decorator);
+	}
+
+	unitDecorator(temp , unitType) {
+		switch (unitType) {
+			case units.CELSIUS:
+				return `${Math.floor(temp - 273.15)} <b>°C</b>`
+			case units.KELVIN:
+				return `${Math.floor(temp)} <b>°K</b>`
+		}
+	}
+
+	setWeather(fn){
+		this.weather=fn;
+		this.weather();
+	}
+
+	onSwitch(){
+		this.weather()
+		switch(this.unitType){
+			case units.CELSIUS:
+				return this.unitType = units.KELVIN
+			case units.KELVIN:
+				return this.unitType = units.CELSIUS
+		}
 	}
 }
 function displayData(el, html, decorator) {
--- app/js/dev/main.js
+++ app/js/dev/main.js
@@ -1,31 +1,33 @@
-const WeatherApp = require('./WeatherApp');
+const WeatherApp = require('./WeatherApp')
 
-const geolocationExist = () => 'geolocation' in navigator;
+const geolocationExist = () => 'geolocation' in navigator
 
 const createUrl = (appidKey) => (lat, lon) =>
-	`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${appidKey}`;
+	`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${appidKey}`
 
 const getPosition = new Promise(
 	(resolve, reject) => geolocationExist() && navigator.geolocation.getCurrentPosition((position) => resolve(position))
-);
+)
 
 const appidKey = '7ad1836fa682671c36d5a02040d194f4';
 
-let weatherApp = new WeatherApp(
+let app = new WeatherApp(
 	document.getElementsByClassName('city-name')[0],
 	document.getElementsByClassName('temperature')[0],
 	document.getElementsByClassName('country-code')[0],
-	document.getElementsByClassName('weather-description')[0]
+	document.getElementsByClassName('weather-description')[0],
+	document.getElementsByClassName('switcher')[0]
 );
 
-getPosition
-	.then((pos) => createUrl(appidKey)(pos.coords.latitude, pos.coords.longitude))
-	.then((url) => fetch(url))
-	.then((response) => response.json())
-	.then((obj) => {
-		weatherApp.setProperty('cityName', obj.name);
-		weatherApp.setProperty('countryCode', obj.sys.country);
-		weatherApp.setProperty('temperature', obj.main.temp, (temp) => `${Math.floor(temp - 273.15)} °C`);
-		weatherApp.setProperty('weatherDescription', obj.weather[0].description, text => text[0].toUpperCase() + text.slice(1)
-		);
-	});
+app.setWeather(() => {
+	getPosition
+		.then((pos) => createUrl(appidKey)(pos.coords.latitude, pos.coords.longitude))
+		.then((url) => fetch(url))
+		.then((response) => response.json())
+		.then((obj) => {
+			app.setProperty('cityName', obj.name)
+			app.setProperty('countryCode', obj.sys.country)
+			app.setProperty('temperature', obj.main.temp, (temp) => app.unitDecorator(temp, app.unitType))
+			app.setProperty('weatherDescription', obj.weather[0].description, (text) => text[0].toUpperCase() + text.slice(1))
+		});
+});
